53297582eb728bff88a5f0ea48c9038ad47f9a15
Arda <ardafincan@icloud.com>
2024-03-04 20:00:54 +0300

    built main structure

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
+
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+
+int main() {
+
+  return 0;
+}
